66edb6f97bd1c15d842fd4b1669195fe2f9be81d
Assane Sow <sowassane478@gmail.com>
2024-09-02 10:23:04 +0000

    chagement mdp

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,4 @@
-import {
-  BadRequestException,
-  HttpException,
-  Injectable,
-} from '@nestjs/common';
+import { BadRequestException, HttpException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { hashFromRequest } from 'src/utils/hash-pass-from-request';
